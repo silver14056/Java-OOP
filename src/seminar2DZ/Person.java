2db202cd346120b33433e64c811ab4c025480524
sergo book <sergo@book.ru>
2024-12-15 21:33:07 +0300

    seminar2:dz start!!!

---
+++ src/seminar2DZ/Person.java
@@ -0,0 +1,71 @@
+package seminar2DZ;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int birthYear;
+    private Person father;
+    private Person mother;
+    private List<Person> children;
+    private String gender;
+
+    public Person(String name, String surname, int birthYear, String gender) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.surname = surname;
+        this.gender = gender;
+        this.children = new ArrayList<>();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+    public void addChildren(Person child) {
+        this.children.add(child);
+        if (child.getGender().equals("male")) {
+            child.setFather(this);
+        } else {
+            child.setMother(this);
+        }
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
